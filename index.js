48704e2a9c2920ce64455ee89e5176fe3a4fa9c8
JSlovak <jasmine.slovak@gmail.com>
2017-05-01 10:43:27 -1000

    chained 3 promise calls using .then

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function getData(index, cb) {
 
 // wrap getData in a promise and console log the
 
-let dataPromise = new Promise((resolve, reject) => {
+new Promise((resolve, reject) => {
   getData(1, (err, data)=>{
     if (err){
       return reject(err);
@@ -21,23 +21,48 @@ let dataPromise = new Promise((resolve, reject) => {
   });
 })
 .then((data)=>{
-  console.log(data);
-})
-.then((data)=>{
-  console.log(data);
-})
-.then((data)=>{
-  console.log(data);
+  //console.log(data);
 });
 
 // call getData three times and console log the output (use .then for the second and third call) using promises
 
-// dataPromise.then(( ) => {
-//   getData();
-// })
-//.catch((reason) => {
-  //console.log('Handle rejected promoise ('+reason+') here.');
-//});e
+new Promise((resolve, reject) => {
+  getData(0, (err, data)=>{
+    if (err){
+      return reject(err);
+    }
+    else {
+      console.log(data);
+      return resolve(data);
+    }
+  });
+})
+.then((data) => {
+  return new Promise((resolve, reject) => {
+    getData(1, (err, data)=>{
+      if (err){
+        return reject(err);
+      }
+      else {
+        console.log(data);
+        return resolve(data);
+      }
+    });
+  });
+})
+.then((data) => {
+  return new Promise((resolve, reject) => {
+    getData(2, (err, data)=>{
+      if (err){
+        return reject(err);
+      }
+      else {
+        console.log(data);
+        return resolve(data);
+      }
+    });
+  });
+});
 
 // create a promise within getData and use the promise to trigger the callback in getData
 
